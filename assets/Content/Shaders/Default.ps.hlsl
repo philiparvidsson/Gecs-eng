7b786dda134201e7961bf5645167a6bb49c6cab2
Philip Arvidsson <philip@philiparvidsson.com>
2016-11-26 19:19:13 +0100

    .

---
+++ assets/Content/Shaders/Default.ps.hlsl
@@ -0,0 +1,43 @@
+﻿/*-------------------------------------
+ * STRUCTS
+ *-----------------------------------*/
+
+struct PS_INPUT {
+    float4 pos      : POSITION0;
+    float4 screenPos: SV_POSITION;
+    float2 texCoord : TEXCOORD0;
+};
+
+struct PS_OUTPUT {
+    float4 color: SV_TARGET;
+};
+
+/*-------------------------------------
+ * CONSTANTS
+ *-----------------------------------*/
+
+SamplerState TextureSampler {
+    AddressU = Wrap;
+    AddressV = Wrap;
+    Filter   = MIN_MAG_MIP_LINEAR;
+};
+
+/*-------------------------------------
+ * GLOBALS
+ *-----------------------------------*/
+
+tbuffer tbTextures {
+    Texture2D Textures[1];
+}
+
+/*-------------------------------------
+ * FUNCTIONS
+ *-----------------------------------*/
+
+float4 tex2D(in Texture2D tex, in float2 texCoord) {
+    return tex.Sample(TextureSampler, texCoord);
+}
+
+void main(in PS_INPUT psIn, out PS_OUTPUT psOut) {
+    psOut.color = tex2D(Textures[0], psIn.texCoord);
+}
